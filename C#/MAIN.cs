f06f6ea095495b81174ff0d47148b2a0a638116a
xCENTx <80198020+xCENTx@users.noreply.github.com>
2021-10-07 16:36:30 -0400

    Almost Complete
    I'd say about 70% cloned C++ source now.
    What's Missing
    - Keybinds 2 and 3

--- C#/MAIN.cs
+++ C#/MAIN.cs
@@ -23,11 +23,53 @@ namespace UniversalNoRecoil2
         //Establish Variables
         static bool _consoleMENU = true;
         static bool bPRESET1 = false;
+        static bool bFLAG = true;
+        static string sPRESET1 = " ", sPRESET2 = " ", sPRESET3 = " ";
+        static string sFLAG = "X";
+
+        //Methods
+        public static void recoilSELECTION(string PRESET1, string PRESET2, string PRESET3, string FLAG)
+        {
+            Console.Clear();
+            Console.WriteLine(" _______________________ \n" +
+            "|-------NO RECOIL-------|\n" +
+            $"| [1] PRESET 1:  => [{PRESET1}] |\n" +
+            $"| [2] PRESET 2:  => [{PRESET2}] |\n" +
+            $"| [3] PRESET 3:  => [{PRESET3}] |\n" +
+            $"| [INS] REQ ADS: => [{FLAG}] |\n" +
+            "| [END] QUIT            |\n" +
+            "|v1.0-------NightFyre---|");
+        }
+        
+        //No Recoil Selections
+        public static void _uniCoil(short aim_key, short shoot_key, int speed, int delay)
+        {
+            if ((aim_key) != 0)
+            {
+                if ((shoot_key) != 0)
+                {
+                    mouse_event(MOUSEEVENTF_MOVE, 0, speed, 0, 0);
+                    Thread.Sleep(delay);
+                }
+            }
+        }
+
+        public static void _uniCoilF(short shoot_key, int speed, int delay)
+        {
+            if ((shoot_key) != 0)
+            {
+                mouse_event(MOUSEEVENTF_MOVE, 0, speed, 0, 0);
+                Thread.Sleep(delay);
+            }
+        }
 
         static void Main(string[] args)
         {
+            Console.Title = "uniCoil";
+            recoilSELECTION(sPRESET1, sPRESET2, sPRESET3, sFLAG);
             while (_consoleMENU)
             {
+                //KeyPresses
                 short keyLMB = GetAsyncKeyState(VK_LBUTTON);
                 short keyRMB = GetAsyncKeyState(VK_RBUTTON);
                 short keyEND = GetAsyncKeyState(VK_END);
@@ -36,38 +78,53 @@ namespace UniversalNoRecoil2
                 short keyNUM2 = GetAsyncKeyState(VK_NUMPAD2);
                 short keyNUM3 = GetAsyncKeyState(VK_NUMPAD3);
 
-                //Keybind Toggle
+                //RECOIL PRESET KEYBINDS
                 if ((keyNUM1 & 1) == 1)
                 {
                     bPRESET1 = !bPRESET1;
                     if (bPRESET1)
                     {
-                        //Key Pressed
-                        Console.WriteLine("PRESET 1 ENABLED - PRESS 1 TO DISABLE or PRESS [END] TO QUIT");
+                        sPRESET1 = "X";
+                        recoilSELECTION(sPRESET1, sPRESET2, sPRESET3, sFLAG);
                     }
                     else
                     {
-                        Console.WriteLine("PRESET 1 DISABLED");
+                        sPRESET1 = " ";
+                        recoilSELECTION(sPRESET1, sPRESET2, sPRESET3, sFLAG);
                     }
                 }
 
-
+                //Quit and Panic Keys
                 if ((keyEND & 1) == 1)
                 {
                     break;
                 }
 
+                if ((keyINSERT & 1) == 1)
+                {
+                    bFLAG = !bFLAG;
+                    if (bFLAG)
+                    {
+                        sFLAG = "X";
+                        recoilSELECTION(sPRESET1, sPRESET2, sPRESET3, sFLAG);
+                    }
+                    else
+                    {
+                        sFLAG = " ";
+                        recoilSELECTION(sPRESET1, sPRESET2, sPRESET3, sFLAG);
+                    }
+                }
 
-                //RecoilEvent
+                //RECOIL EVENTS
                 if (bPRESET1)
                 {
-                    if ((keyRMB) != 0)
+                    if (bFLAG)
                     {
-                        if ((keyLMB) != 0)
-                        {
-                            mouse_event(MOUSEEVENTF_MOVE, 0, 7, 0, 0);
-                            Thread.Sleep(7);
-                        }
+                        _uniCoil(keyRMB, keyLMB, 7, 7);
+                    }
+                    else
+                    {
+                        _uniCoilF(keyLMB, 7, 7);
                     }
                 }
             }
